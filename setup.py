c35fb862881b40f92312cad977998d8e50ac4598
Jiovani Ledesma Arredondo <jiovaniledesmaa@gmail.com>
2019-04-06 15:58:35 -0600

    Actualizado

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='holographicM',
+      version='0.1.0',
+      description='Python library for an Holographic Microscope',
+      author='IRO-UPGTO',
+      author_email='jiovaniledesmaa@gmail.com',
+      license = "MIT",
+      keywords=["Holography","Microscope","Holographic Microscope"],
+      url='https://github.com/JiovaniLedesma/holographicM',
+      packages=['HolographicMicroscope'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
